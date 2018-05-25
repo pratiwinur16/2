058d671fab7b6cd1cd0843352a9e29e8c7a7ed3f
pratiwi <pratiwinur.amini@gmail.com>
2018-05-25 11:39:46 +0700

    [Exercises 13] X dan O

---
+++ Exercise-13.js
@@ -0,0 +1,23 @@
+function xo(str) {
+  var jumlahX= 0;
+  var jumlahO= 0;
+  for (var i=0;  i<str.length; i++){
+    if  (str[i] === 'x'){
+      jumlahX++;
+    } else if (str[i] === 'o'){
+       {
+        jumlahO++;
+      }
+    }
+  }
+  if (jumlahX==jumlahO){
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
